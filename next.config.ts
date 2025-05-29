8118be44479ebd1857d6dbc6a2c0d4a2ff777d79
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2025-05-29 09:34:19 -0300

    Componente da pizza

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http", // Without colon
+        hostname: "localhost",
+        port: "3000",
+        pathname: "/**",  // Wildcard for all images
+      }
+    ]
+  }
 };
 
 export default nextConfig;
---
+++ src/components/home/pizza-item.tsx
@@ -0,0 +1,40 @@
+"use client"
+
+import { Product } from "@/generated/prisma";
+import Image from "next/image";
+import { Button } from "../ui/button";
+
+type Props = {
+    data: Product
+}
+
+export const PizzaItem = ({ data }: Props) => {
+
+    const handleAddToCart = () => {
+
+    }
+
+    return (
+        <div className="text-sm bg-secondary p-4 rounded-md">
+            <Image 
+                src={data.image}
+                alt={data.name}
+                width={200}
+                height={200}
+                className="w-full mb-3 rounded-md"
+            />
+            <div className="text-lg font-bold">
+                {data.name}
+            </div>
+            <div>
+                {data.price.toString()}
+            </div>
+            <div className="truncate mb-3">
+                {data.ingredients}
+            </div>
+            <div className="text-center">
+                <Button onClick={handleAddToCart}>Adicionar ao Carrinho</Button>
+            </div>
+        </div>
+    );
+}
