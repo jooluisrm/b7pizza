94f04c014b6fe9700ece3e9e2be0b4b3a1f12ff8
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2025-05-29 09:10:17 -0300

    Configurando o axios para consultar API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import { Header } from "@/components/layout/header";
+import { api } from "@/lib/axios";
 
 export default async function Page() {
+
+    //const pizzaReq = await api.get('/pizzas');
+
     return (
         <div>
             <Header />
