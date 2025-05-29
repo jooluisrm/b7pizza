c36e6b49da170a096a68bb5b5d746b0fe3d80e53
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2025-05-29 08:57:32 -0300

    Criando base do layout

---
+++ src/components/layout/header.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import Link from "next/link";
+
+export const Header = () => {
+    return (
+        <header className="container mx-auto flex my-4 p-5 items-center justify-between bg-secondary rounded-md">
+            <Link href={'/'}>
+                <div className="text-2xl font-bold">
+                    B7Pizza
+                </div>
+            </Link>
+            <div className="flex gap-2">
+                <button>Login/Cadastro</button>
+                <button>Carrinho</button>
+            </div>
+        </header>
+    );
+}
