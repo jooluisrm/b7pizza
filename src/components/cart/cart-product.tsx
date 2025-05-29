7d4d47dbbacedf5551d9a0009aa90f5fb1ce6a88
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2025-05-29 18:52:22 -0300

    Alterando quantidades no carrinho

--- src/components/cart/cart-product.tsx
+++ src/components/cart/cart-product.tsx
@@ -6,6 +6,7 @@ import { CartItem } from "@/types/cart-item";
 import Image from "next/image";
 import { Button } from "../ui/button";
 import { useState } from "react";
+import { useCart } from "@/stores/cart";
 
 type Props = {
     data: CartItem;
@@ -15,14 +16,30 @@ export const CartProduct = ({ data }: Props) => {
 
     const [qt, setQt] = useState(data.quantity);
 
+    const cart = useCart();
+
     const products = useProducts();
     let product = products.products.find(item => item.id === data.productId);
-    if(!product) return null;
+    if (!product) return null;
+
+    const handleMinusClick = () => {
+        if (qt - 1 <= 0) {
+            cart.removeItem(data.productId);
+        } else {
+            cart.addItem({ productId: data.productId, quantity: -1 });
+            setQt(qt - 1)
+        }
+    }
+
+    const handlePlusClick = () => {
+        cart.addItem({ productId: data.productId, quantity: 1 });
+        setQt(qt + 1);
+    }
 
     return (
         <div className="flex items-center gap-3">
             <div className="w-10">
-                <Image 
+                <Image
                     alt={product.name}
                     src={product.image}
                     width={100}
@@ -35,9 +52,9 @@ export const CartProduct = ({ data }: Props) => {
                 <div className="text-sm">{decimalToMoney(product.price)}</div>
             </div>
             <div className="flex items-center bg-secondary p-2 rounded-md">
-                <Button size={"sm"} variant={'ghost'} className="border-r rounded-none">-</Button>
+                <Button onClick={handleMinusClick} size={"sm"} variant={'ghost'} className="border-r rounded-none">-</Button>
                 <div className="mx-3">{qt}</div>
-                <Button size={"sm"} variant={'ghost'} className="border-l rounded-none" >+</Button>
+                <Button onClick={handlePlusClick} size={"sm"} variant={'ghost'} className="border-l rounded-none" >+</Button>
             </div>
         </div>
     );
