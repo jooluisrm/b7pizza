c0be24bcede264dd07589a8387a7aa045e644996
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2025-06-03 16:40:05 -0300

    Ajustando pedido para o Stripe

--- src/app/api/order/new/route.ts
+++ src/app/api/order/new/route.ts
@@ -1,8 +1,11 @@
 import { getLoggedUserFromHeader } from "@/services/auth";
 import { createNewOrder } from "@/services/order";
+import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
+    const headersList = await headers();
+    const origin = headersList.get('origin');
 
     const { cart } = await request.json();
     const loggedUser = await getLoggedUserFromHeader();
@@ -11,8 +14,7 @@ export async function POST(request: Request) {
     if (!cart || (cart && cart.lenght <= 0)) return NextResponse.json({ error: "Carrinho vazio" });
 
     const order = await createNewOrder(loggedUser.id, cart);
+    if (!order) return NextResponse.json({ error: "Ocorreu um erro" });
 
-    if(!order) return NextResponse.json({error: "Ocorreu um erro"});
-
-    return NextResponse.json({ blabla: true });
+    return NextResponse.json({ order }, { status: 201 });
 }
