1a775d66053300c38e95bc9786656db84450e6d6
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2025-06-03 08:17:04 -0300

    Sincronizando o Auth Store

--- src/components/login-area/login-area-dialog.tsx
+++ src/components/login-area/login-area-dialog.tsx
@@ -2,11 +2,12 @@
 
 import { useAuth } from "@/stores/auth";
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "../ui/dialog";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "../ui/button";
 import { ArrowLeft } from "lucide-react";
 import { LoginAreaStepEmail } from "./login-area-step-email";
 import { LoginAreaStepSignup } from "./login-area-step-signup";
+import { getCookie } from "cookies-next/client";
 
 type Steps = "EMAIL" | "SIGUP" | "SIGNIN";
 
@@ -16,6 +17,11 @@ export const LoginAreaDialog = () => {
 
     const [step, setStep] = useState<Steps>("EMAIL");
     const [emailFild, setEmailField] = useState('');
+
+    useEffect(() => {
+        const token = getCookie('token');
+        if(token) auth.setToken(token);
+    }, []);
 
     const handleStepEmail = (hasEmail: boolean, email: string) => {
         setEmailField(email);
