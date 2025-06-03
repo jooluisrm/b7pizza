17561b08546d84889c8d0d9849c1894872823678
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2025-06-03 16:56:58 -0300

    Stripe-Hosted: Criando uma sessão de pagamento

--- src/app/api/order/new/route.ts
+++ src/app/api/order/new/route.ts
@@ -1,9 +1,11 @@
+import { stripe } from "@/lib/stripe";
 import { getLoggedUserFromHeader } from "@/services/auth";
 import { createNewOrder } from "@/services/order";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
+
     const headersList = await headers();
     const origin = headersList.get('origin');
 
@@ -16,5 +18,35 @@ export async function POST(request: Request) {
     const order = await createNewOrder(loggedUser.id, cart);
     if (!order) return NextResponse.json({ error: "Ocorreu um erro" });
 
-    return NextResponse.json({ order }, { status: 201 });
+    // Metodo de pagamento
+    const paymentSession = await stripe.checkout.sessions.create({
+        mode: 'payment',
+        success_url: `${origin}/success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${origin}`,
+        line_items: [
+            {
+                price_data: {
+                    currency: 'BRL',
+                    unit_amount: 100,
+                    product_data: {
+                        name: 'Produto de Teste',
+                        description: 'Descriçao bobobobo'
+                    }
+                },
+                quantity: 1
+            },
+            {
+                price_data: {
+                    currency: 'BRL',
+                    unit_amount: 12345,
+                    product_data: {
+                        name: 'Produto de Teste 2'
+                    }
+                },
+                quantity: 2
+            }
+        ]
+    });
+
+    return NextResponse.json({ order, url: paymentSession.url }, { status: 201 });
 }
