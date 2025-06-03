a03f86dfa8df181f085bca386f619bf09a32346e
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2025-06-03 19:08:38 -0300

    Modificando status da compra

--- src/app/api/webhook/stripe/route.ts
+++ src/app/api/webhook/stripe/route.ts
@@ -1,8 +1,51 @@
+import { prisma } from "@/lib/prisma";
+import { stripe } from "@/lib/stripe";
+import { headers } from "next/headers";
 import { NextResponse } from "next/server";
+import Stripe from "stripe";
 
 export async function POST(request: Request) {
+    const headersList = await headers();
+    const stripeSignature = headersList.get('stripe-signature');
+    const rawBody = await request.text();
 
-    console.log('ok');
 
-    return NextResponse.json({bla: true});
+    try {
+        const event = stripe.webhooks.constructEvent(
+            rawBody,
+            stripeSignature!,
+            process.env.STRIPE_WEBHOOK_KEY!
+        );
+
+        const eventTypes = [
+            `checkout.session.completed`,
+            `checkout.session.async_payment_succeeded`
+        ]
+
+        if (eventTypes.includes(event.type)) {
+            const { metadata, payment_status } = event.data.object as any;
+
+            if (payment_status === 'paid') {
+                const orderId = parseInt(metadata.order_id);
+                
+                if (orderId) {
+                    const order = await prisma.order.findUnique({
+                        where: { id: orderId }
+                    });
+                    if (order) {
+                        await prisma.order.update({
+                            where: { id: orderId },
+                            data: { status: 'PAID' }
+                        });
+                    }
+                }
+            }
+        }
+    } catch (error: any) {
+        console.log(error.message)
+        return NextResponse.json({ error: `Webhook Error: ${error.message}` }, { status: 400 });
+    }
+
+
+    return NextResponse.json({});
 }
